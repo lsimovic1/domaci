fdf02f8b85086812cacd74bc0b4e97b23e1e2b8a
Lazar Simovic <ls20190093@student.fon.bg.ac.rs>
2022-11-16 13:03:49 +0100

    Prikaz i pocetak ajaxa

---
+++ voznja/crud/prikaz.php
@@ -0,0 +1,43 @@
+<?php
+
+require "../../DB.php";
+$db = new DB('domaci');
+?>
+
+<table class="table table-bordered mt-2">
+    <thead>
+        <tr class="text-center">
+            <th>ID</th>
+            <th>Naziv kompanije</th>
+            <th>Pocetna destinacija</th>
+            <th>Krajnja destinacija</th>
+            <th>Vreme voznje</th>
+            <th>Operacije</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php
+
+        $query = "select * from kompanija";
+        $data = $db->connection->query($query);
+
+        while ($row = $data->fetch_object()) :
+        ?>
+            <tr class="text-center">
+                <td><?php echo $row->id; ?></td>
+                <td><?php echo $row->naziv;  ?></td>
+                <td><?php echo $row->pocetna;  ?></td>
+                <td><?php echo $row->krajnja;  ?></td>
+                <td><?php echo $row->vreme;  ?></td>
+                <td>
+                    <button class="btn btn-primary" id="btn_edit" value="<?php echo $row->id; ?>">Izmeni</button>
+                    <button class="btn btn-danger" id="btn_delete" value="<?php echo $row->id; ?>">Obriši</button>
+                </td>
+            </tr>
+        <?php endwhile; ?>
+
+    </tbody>
+
+
+</table>
---
+++ voznja/voznja.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    prikaziVoznje();
+    dodajVoznju();
+});
+
+function prikaziVoznje() {
+
+    $.ajax(
+        {
+            url: 'crud/prikaz.php',
+            success: function (data) {
+                {
+                    $('#tabelaVoznja').html(data);
+                }
+            }
+        }
+    )
+}
+
+function dodajVoznju() {
+
+    $(document).on('click', '#btn_save', function () {
+
+        var naziv = $('#addnaziv').val();
+        var pocetna = $('#addpocetna').val();
+        var krajnja = $('#addkrajnja').val();
+        var vreme = $('#addvreme').val();
+
+
+        if (naziv == '' || pocetna == '' || krajnja == '' || vreme == '') {
+            $('#praznaPolja').slideDown().delay(1500).fadeOut('slow');
+        }
+        else {
+            $.ajax(
+                {
+                    url: 'crud/save.php',
+                    method: 'post',
+                    data: { naziv: naziv, pocetna: pocetna, krajnja: krajnja,vreme: vreme },
+
+                    success: function (data) {
+                        $('#praznaPolja').hide();
+                        $('#uspesnoSacuvan').fadeIn().html(data).delay(1800).fadeOut('slow');
+
+                        prikaziVoznje();
+
+                        $('#addnaziv').val('');
+                        $('#addpocetna').val('');
+                        $('#addkrajnja').val('');
+                        $('#addvreme').val('');
+
+
+                    }
+                });
+        }
+    })
+}
