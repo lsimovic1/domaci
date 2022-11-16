e5d3dc30052fcdc58b01642d833c439572007702
Lazar Simovic <ls20190093@student.fon.bg.ac.rs>
2022-11-16 12:36:38 +0100

    Broker

---
+++ DB.php
@@ -0,0 +1,22 @@
+<?php
+
+class DB
+{
+    public $host = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $db = "domaci";
+    public $connection;
+
+    function __construct($db)
+    {
+        $this->db = $db;
+        $this->connect();
+    }
+
+    function connect()
+    {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->db);
+    }
+}
+?>
