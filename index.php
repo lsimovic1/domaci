287d38292a94a5cb5458c4cd23a8533598e706cc
Lazar Simovic <ls20190093@student.fon.bg.ac.rs>
2022-11-19 22:07:40 +0100

    Kraj

--- index.php
+++ index.php
@@ -8,13 +8,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css">
     <title>Početna</title>
+  
+    <style>body {
+    background-image: url('img/slika1.jpg');
+    background-size: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    } </style>
 </head>
 
 <body>
 
     <div class="container">
 
-        <h1>Dobrodošli!<br>Autobuska stanica Beograd</h1>
+        <h1 style="color:white;">Dobrodošli!<br>Autobuska stanica Beograd</h1>
 
         <div class="putnicidiv">
             <a href="putnik/index.php"><button class="btn btn-primary" id="crudputnici">Putnici</button></a>
